96832c88d15169db0f2d0e638e45d8294698a622
FaizalKhan-AT <faizalkhanfk148@gmail.com>
2021-10-12 22:24:35 -0700

    some more updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router , Route} from 'react-router-dom';
+import {HashRouter as Router , Route} from 'react-router-dom';
 import Home from './Pages/Home' 
 import Contexts from './store/Contexts'
 import Watch from './Pages/watchPage' 
